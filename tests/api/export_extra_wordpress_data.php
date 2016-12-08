ea572f5ffdf9491c8dd0a9070ed89e2f3a645e41
jan <jan.s@icanlocalize.com>
2016-12-08 11:08:10 +0100

    A very basic unit test of the toolset_export_extra_wordpress_data API filter.

---
+++ tests/api/export_extra_wordpress_data.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ToolsetExtraExport\Tests\Api;
+use ToolsetExtraExport\Tests as tests;
+
+class Export_Extra_Wordpress_Data extends tests\Test_Case {
+
+    const FILTER_NAME = 'toolset_export_extra_wordpress_data';
+
+
+    function test_export_filter_works() {
+        // passing invalid argument, we should get an empty array of results
+        $this->assertTrue( is_array( apply_filters( self::FILTER_NAME, null ) ) );
+    }
+
+
+    function get_default_values() {
+        return [
+            [
+                'section_name' => 'settings_reading',
+                'default_value' => [
+                    'settings_reading' => [
+                        'settings' => [
+                            'blog_charset' => 'UTF-8',
+                            'show_on_front' => 'posts',
+                            'page_on_front' => 0,
+                            'page_for_posts' => 0,
+                            'posts_per_page' => 10,
+                            'post_per_rss' => 10,
+                            'rss_use_excerpt' => 0,
+                            'blog_public' => 1
+                        ],
+                        'portable' => [
+                            'page_on_front' => [ 'exists' => false ],
+                            'page_for_posts' => [ 'exists' => false ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+    }
+
+
+    /**
+     * @dataProvider get_default_values
+     *
+     * @param $section_name
+     * @param $expected_default_value
+     */
+    function test_default_values( $section_name, $expected_default_value ) {
+
+        $output = apply_filters( self::FILTER_NAME, null, [ $section_name ] );
+
+        $this->assertNotNull( $output );
+
+        $this->assertEquals( $expected_default_value, $output, 'Default values are not equal', 0, 10, true );
+
+    }
+}
