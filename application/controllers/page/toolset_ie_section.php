353a68784adf8598f27bace792f52b48519d793b
jan <jan.s@icanlocalize.com>
2017-01-03 17:18:21 +0100

    Hide the import GUI by default (until we make the plugin fully standalone).

--- application/controllers/page/toolset_ie_section.php
+++ application/controllers/page/toolset_ie_section.php
@@ -40,22 +40,35 @@ class Toolset_Ie_Section extends Page_Import_Export {
 
     	$self = $this;
 
+    	$tab_items = [
+		    'export' => [
+			    'slug' => self::EXPORT_SECTION_SLUG,
+			    'title' => __( 'Export Theme settings', 'toolset-ee' ),
+			    'callback' => function() use( $self ) { $self->render( 'export' ); }
+		    ]
+	    ];
+
+	    /**
+	     * toolset_extra_export_show_import_gui
+	     *
+	     * Allow for showing or hiding the Import part of the GUI on the Toolset Export / Import page.
+	     *
+	     * @param bool $show_import_gui
+	     * @since 1.0
+	     */
+    	if( apply_filters( 'toolset_extra_export_show_import_gui', false ) ) {
+    		$tab_items['import'] = [
+			    'slug' => self::IMPORT_SECTION_SLUG,
+			    'title' => __( 'Import Theme settings', 'toolset-ee' ),
+			    'callback' => function() use( $self ) { $self->render( 'import' ); }
+		    ];
+	    }
+
         $sections[ self::TAB_SLUG ] = [
             'slug' => self::TAB_SLUG,
             'title' => __( 'Theme (TBT)', 'toolset-ee' ),
             'icon' => '<i class="icon-toolset-logo ont-icon-16"></i>', // todo add specific icon
-            'items' => [
-                'export' => [
-                    'slug' => self::EXPORT_SECTION_SLUG,
-                    'title' => __( 'Export Theme settings', 'toolset-ee' ),
-                    'callback' => function() use( $self ) { $self->render( 'export' ); }
-                ],
-                'import' => [
-	                'slug' => self::IMPORT_SECTION_SLUG,
-	                'title' => __( 'Import Theme settings', 'toolset-ee' ),
-	                'callback' => function() use( $self ) { $self->render( 'import' ); }
-                ]
-            ]
+            'items' => $tab_items
         ];
 
         return $sections;
